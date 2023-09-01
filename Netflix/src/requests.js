d1770400ff6908285428b673976b41639cf5824e
Binyam914 <binyamaregwi6@gmail.com>
2023-09-01 04:10:30 -0700

    Completed Netflix-Clone project

---
+++ Netflix/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY="005af750b5e857c004bd5330ebe245e8"
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchUpcomming: `/movie/upcoming?api_key=${API_KEY}&language=eng&page=1`,
+    fetchPlayknow: `/movie/now_playing?api_key=${API_KEY}&language=eng&page=44`
+};
+export default requests;
